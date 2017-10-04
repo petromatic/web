46d0a31a96ebc07a703623b9e797b2f2daf7192c
Nicolás Dazeo <nicodaze@gmail.com>
2017-10-04 18:41:08 -0300

    User edit and update working

--- src/app/useredit/useredit.component.ts
+++ src/app/useredit/useredit.component.ts
@@ -11,18 +11,47 @@ export class UsereditComponent implements OnInit {
   id: string;
   userModel: FirebaseObjectObservable<any>;
   user: any;
+  changed: boolean = false;
 
   constructor(private route: ActivatedRoute, private db: AngularFireDatabase) {
     this.route.params.subscribe(params => {
       this.id = params['id'];
-      this.userModel = this.db.object('/users/'+this.id, { preserveSnapshot: true });
-      this.userModel.subscribe( user => {
-        this.user = user.val()
-      });
+      if(this.id)
+      {
+        this.userModel = this.db.object('/users/'+this.id, { preserveSnapshot: true });
+        console.log(this.userModel);
+        this.userModel.subscribe( user => {
+          this.user = user.val()
+        });
+      }
+      else
+      {
+        this.user = {};
+      }
    });
    }
 
   ngOnInit() {
   }
 
+  save()
+  {
+    console.log("save");
+    if(!this.id)
+    {
+      console.log("New");
+      this.db.list('/users').push(this.user).then( (user) => {
+        console.log(user);
+        this.id = user.key;
+        this.userModel = this.db.object('/users/'+this.id, { preserveSnapshot: true });
+        this.changed = false;
+      });
+    }
+    else
+    {
+      this.userModel.update(this.user).then( (user) =>{
+        this.changed = false;
+      });
+    }
+  }
 }
