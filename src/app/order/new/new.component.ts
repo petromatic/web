ac7e3b51989d58d765e2ad091333c2589964a483
Nicolás Dazeo <nicodaze@gmail.com>
2018-01-10 20:38:22 -0300

    Order creation pulls trucks and drivers lists and check for full tank.

--- src/app/order/new/new.component.ts
+++ src/app/order/new/new.component.ts
@@ -14,17 +14,24 @@ export class NewOrderComponent implements OnInit {
   order: any;
   amount: number = 0;
   changed: boolean = false;
+  full: boolean = false;
+  capacity: number = 0;
+  trucks: Observable<any[]>;
+  drivers: Observable<any[]>;
 
   constructor(private location: Location, private route: ActivatedRoute, public db: AngularFireDatabase) {
     this.order = {
-      truck: '-L1Ju4JdIm63ciTdqA6F',
-      driver:'-L1JxWQalVKFrpY1iqCq'
     };
     this.route.params.subscribe(params => {
       this.userId = params['uid'];
       if(this.userId)
       {
-
+        this.drivers = this.db.list('/user_drivers/'+this.userId).snapshotChanges().map(changes => {
+          return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+        });
+        this.trucks = this.db.list('/user_trucks/'+this.userId).snapshotChanges().map(changes => {
+          return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+        });
       }
       else
       {
@@ -33,6 +40,11 @@ export class NewOrderComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+
+  onTruckChange(event){
+    const sel = event.target;
+    this.capacity = parseInt(sel.options[sel.selectedIndex].dataset.capacity);
   }
 
   save()
